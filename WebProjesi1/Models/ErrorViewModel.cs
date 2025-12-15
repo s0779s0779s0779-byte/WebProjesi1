8779013111943a75d5a7fb9fbe15bbe289a4d802
shhada <shhada@MSI>
2025-12-15 23:53:02 +0300

    Add project files.

---
+++ WebProjesi1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebProjesi1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
